f3efd99d00ee4eee8cf4074ef26f9210ac5ec625
Devin Frenze <frenze.devin@gmail.com>
2020-04-08 13:56:54 -0400

    add local redis initialization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import redis from 'redis'
 
 const app = express()
 const port = 3000
 
+const redisClient = redis.createClient()
+
+redisClient.on('connect', () => {
+  console.log('Redis client connected')
+  app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+})
+
+redisClient.on('error', (err) => {
+  console.log(`Redis client error ${err}`)
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
